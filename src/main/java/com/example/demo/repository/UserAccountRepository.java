f0711ef52651105962fbd00aa7da88d040c8417f
bala-amypo <bala@amypo.in>
2025-12-25 14:39:45 +0000

    User commit

--- src/main/java/com/example/demo/repository/UserAccountRepository.java
+++ src/main/java/com/example/demo/repository/UserAccountRepository.java
@@ -1,11 +1,11 @@
 package com.example.demo.repository;
 
-import com.example.demo.model.TeamCapacityConfig;
+import com.example.demo.model.UserAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface TeamCapacityConfigRepository extends JpaRepository<TeamCapacityConfig, Long> {
-    Optional<TeamCapacityConfig> findByTeamName(String teamName);
+public interface UserAccountRepository extends JpaRepository<UserAccount, Long> {
+    Optional<UserAccount> findByEmail(String email);
 }
