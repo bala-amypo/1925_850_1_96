0d1097a490fdabd4d0bb79df8d867cd2fd158467
bala-amypo <bala@amypo.in>
2025-12-25 14:29:38 +0000

    User commit

--- src/main/java/com/example/demo/config/OpenApiConfig.java
+++ src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -1,11 +1,10 @@
 package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
@@ -13,15 +12,9 @@ public class OpenApiConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                .info(new Info()
-                        .title("Leave Overlap & Team Capacity Analyzer API")
-                        .description("API for managing employee leave requests and analyzing team capacity")
-                        .version("1.0.0"))
-                .addSecurityItem(new SecurityRequirement().addList("JWT"))
-                .components(new io.swagger.v3.oas.models.Components()
-                        .addSecuritySchemes("JWT", new SecurityScheme()
-                                .type(SecurityScheme.Type.HTTP)
-                                .scheme("bearer")
-                                .bearerFormat("JWT")));
-    }
+                // You need to change the port as per your server
+                .servers(List.of(
+                        new Server().url("https://9260.408procr.amypo.ai/")
+                ));
+        }
 }
