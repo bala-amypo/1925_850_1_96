e3d3e6158454ce9b9e4d526684b76aa82804d444
bala-amypo <bala@amypo.in>
2025-12-26 12:35:10 +0000

    User commit

--- src/main/java/com/example/demo/config/OpenApiConfig.java
+++ src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -1,23 +1,23 @@
-// package com.example.demo.config;
+package com.example.demo.config;
 
-// import io.swagger.v3.oas.models.OpenAPI;
-// import io.swagger.v3.oas.models.servers.Server;
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-// import java.util.List;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import java.util.List;
 
-// @Configuration
-// public class OpenApiConfig {
+@Configuration
+public class OpenApiConfig {
 
-//     @Bean
-//     public OpenAPI customOpenAPI() {
-//         return new OpenAPI()
-//                 // You need to change the port as per your server
-//                 .servers(List.of(
-//                         new Server().url("https://9226.408procr.amypo.ai/")
-//                 ));
-//         }
-// }
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                // You need to change the port as per your server
+                .servers(List.of(
+                        new Server().url("https://9226.408procr.amypo.ai/")
+                ));
+        }
+}
 
 
 
@@ -60,27 +60,27 @@
 
 
 
-package com.example.demo.config;
+// package com.example.demo.config;
 
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+// import io.swagger.v3.oas.models.Components;
+// import io.swagger.v3.oas.models.OpenAPI;
+// import io.swagger.v3.oas.models.security.SecurityRequirement;
+// import io.swagger.v3.oas.models.security.SecurityScheme;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
 
-@Configuration
-public class OpenApiConfig {
+// @Configuration
+// public class OpenApiConfig {
 
-    @Bean
-    public OpenAPI customOpenAPI() {
-        return new OpenAPI()
-                .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
-                .components(new Components().addSecuritySchemes("Bearer Authentication", 
-                    new SecurityScheme()
-                        .name("Bearer Authentication")
-                        .type(SecurityScheme.Type.HTTP)
-                        .scheme("bearer")
-                        .bearerFormat("JWT")));
-    }
-}
+//     @Bean
+//     public OpenAPI customOpenAPI() {
+//         return new OpenAPI()
+//                 .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+//                 .components(new Components().addSecuritySchemes("Bearer Authentication", 
+//                     new SecurityScheme()
+//                         .name("Bearer Authentication")
+//                         .type(SecurityScheme.Type.HTTP)
+//                         .scheme("bearer")
+//                         .bearerFormat("JWT")));
+//     }
+// }
